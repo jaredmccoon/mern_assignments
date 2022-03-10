4ecd62acd2ec3abb3dd078954cab848ed429eaca
Jared McCoon <jaredmccoon@protonmail.com>
2022-03-09 18:45:11 -0800

    first_jsx

---
+++ react/intro_to_react/first_jsx/first_jsx/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {    
+    return (       
+        <>
+        <h1>Hello World</h1>    
+        <h3>Todo list</h3>
+        <ul>
+          <li>item1</li>
+          <li>item2</li>
+          <li>item3</li>
+          <li>item4</li>
+        </ul>
+        </>     
+    
+    );    
+}
+
+export default App;
